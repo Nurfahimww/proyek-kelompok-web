4422593cd5440015df9edb3e68a4be12ec292d00
Nurfahimww <79034715+Nurfahimww@users.noreply.github.com>
2021-06-03 14:36:13 +0700

    file mentahan tampoilan
    tampilan awal web

---
+++ tampilan.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> juallaptop.com</title>
+	</head>
+		<body>
+
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+			<div class="container">
+
+			
+
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  			<!-- Brand -->
+  			<a class="navbar-brand" href="#">Logo</a>
+
+  			<!-- Links -->
+  			<ul class="navbar-nav">
+    			
+
+    			<!-- Dropdown -->
+    			<li class="nav-item dropdown">
+      				<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        			laptop
+      				</a>
+      					<div class="dropdown-menu">
+       				 		<a class="dropdown-item" href="#">Link 1</a>
+        					<a class="dropdown-item" href="#">Link 2</a>
+        					<a class="dropdown-item" href="#">Link 3</a>
+      					</div>
+    			</li>
+  			</ul>
+		</nav>
+
+		</div>
+
+		</body>
+
+</html>
